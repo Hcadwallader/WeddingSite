922d19ede052ab9910419c6e1d3bda8b01760716
Helen Cadwallader <helencadwallader3@gmail.com>
2020-03-11 09:01:48 +0000

    beginnings of hooking up api with frontend

--- src/components/ApiTest/ApiTest.js
+++ src/components/ApiTest/ApiTest.js
@@ -1,22 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 const ApiTest = () => {
 
-    const [apiResponse, setApiResponse] = useState(null);
+        const [apiResponse, setApiResponse] = useState(null);
 
-    useEffect(() => {
-        fetch('http://localhost:8080/api')
-            .then((res) => res.json())
-            .then((data) => setApiResponse(data.Message));
-    }, []);
+        useEffect(() => {
 
-    return (
-        <div>
-            <br/>
-            <p>{apiResponse}</p>
-        </div>
-    );
+            const guest = {
+                firstName: 'Ceri',
+                surname: 'Groves',
+                attending: true
+            };
+            // fetch('http://localhost:8080/api/guests?name=Ceri')
+            //     .then((res) => res.json())
+            //     .then((data) => setApiResponse(data.Message));
 
-};
+
+            fetch('http://localhost:8080/api/guests', {
+                method: 'POST', // or 'PUT'
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify(guest),
+            })
+                .then((res) => res.json())
+                .then((data) => setApiResponse(data.Message));
+
+            ;
+        }, [])
+        ;
+
+        return (
+            <div>
+                <br/>
+                <p>{apiResponse}</p>
+            </div>
+        );
+
+    }
+;
 
 export default ApiTest;
