9c99e4927500e26fb5cc7b175a8f7fbb8bc1a4b6
Helen Cadwallader <helencadwallader3@gmail.com>
2020-04-08 16:52:35 +0100

    add styles to input boxes and placeholder form fields

--- src/components/Rsvp/Rsvp.js
+++ src/components/Rsvp/Rsvp.js
@@ -1,45 +1,85 @@
-import React, {useState, useEffect} from "react";
-import "../../styles/App.scss";
+import React, { useState, useEffect } from 'react';
+import '../../styles/App.scss';
+import './Rsvp.scss';
 
-const guestDataService = process.env.NODE_ENV !== 'production'
-    ? require("../../services/DummyGuestDataService")
-    : require("../../services/GuestDataService");
+const guestDataService =
+	process.env.NODE_ENV !== 'production'
+		? require('../../services/DummyGuestDataService')
+		: require('../../services/GuestDataService');
 
 const Rsvp = () => {
+	const [guestlist, setGuest] = useState([]);
+	const [guestResponse, setGuestResponse] = useState([]);
 
-    const [guest, setGuest] = useState([]);
+	useEffect(() => {
+		const guests = guestDataService.GetGuestList();
+		console.log(guests);
+		setGuest(guests);
+	}, []);
 
-    useEffect(() => {
-        const guests = guestDataService.GetGuestList();
-        console.log(guests);
-        setGuest(guests);
-    },[]);
+	/*const lookupGuestOnInviteList = (valueEntered) => {
+       const guestfound = guestlist.find(g => g.name > valueEntered)
+       const listOfRelatedGuests = guestlist.filter(g => g.relatives.)
+    }
+*/
+	const handleGuestChange = (e) => {
+		setGuestResponse({
+			...guestResponse,
+			[e.target.name]: [e.target.value],
+		});
+	};
 
-    const handleGuestChange = (e) => setGuest({
-        ...guest,
-        [e.target.name]: [e.target.value],
-    });
-
-    return (
-        <div className="whiteBox">
-            <h1>RSVP form</h1>
-            <form>
-                <label htmlFor="name">Name</label>
+	return (
+		<div className="whiteBox">
+			<h1>RSVP form</h1>
+			<form>
+				<input
+					type="text"
+					name="name"
+					id="name"
+					value={guestResponse.name}
+					onChange={handleGuestChange}
+					placeholder="Name"
+				/>
                 <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={guest.name}
-                    onChange={handleGuestChange}
-                />
-                {guest && Object.values(guest).map((g, index) => {
-                    return <p key={index}>{g.name}</p>;
-                })}
-                {/* <input type="button" value="Add New Cat" />
+					type="text"
+					name="name"
+					id="name"
+					value={guestResponse.name}
+					onChange={handleGuestChange}
+					placeholder="Name"
+				/>
+                <input
+					type="text"
+					name="name"
+					id="name"
+					value={guestResponse.name}
+					onChange={handleGuestChange}
+					placeholder="Name"
+				/>
+                <input
+					type="text"
+					name="name"
+					id="name"
+					value={guestResponse.name}
+					onChange={handleGuestChange}
+					placeholder="Name"
+				/>
+                <input
+					type="text"
+					name="songchoice"
+					id="songchoice"
+					placeholder="Enter a great song for our evening playlist (optional)"
+				/>
+				{/* {guestlist &&
+					Object.values(guestlist).map((g, index) => {
+						return <p key={index}>{g.name}</p>;
+					})} */}
+				{/* <input type="button" value="Add New Cat" />
         <input type="submit" value="Submit" />*/}
-            </form>
-        </div>
-    );
-}
+			</form>
+		</div>
+	);
+};
 
 export default Rsvp;
