591b99125f9c213a044da9fb59a449bd160b8129
Helen Cadwallader <helencadwallader3@gmail.com>
2020-04-08 16:51:38 +0100

    Add custom fonts

--- webpack.common.js
+++ webpack.common.js
@@ -1,43 +1,54 @@
 const path = require('path');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: "./src/index.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-                options: {presets: ["@babel/env"]}
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(jpg|png)$/,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        name: "[path][name].[hash].[ext]",
-                    },
-                },
-            }
-        ]
-    },
-    resolve: {extensions: ["*", ".js", ".jsx"]},
-    output: {
-        path: path.resolve(__dirname, "dist/"),
-        publicPath: "/dist/",
-        filename: "[name].bundle.js",
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Production',
-        })]
+	entry: {
+		app: './src/index.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				options: { presets: ['@babel/env'] },
+			},
+			{
+				test: /\.(scss|css)$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(jpg|png)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[path][name].[hash].[ext]',
+					},
+				},
+			},
+			{
+				test: /\.ttf$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						name: './fonts/[name].[ext]',
+						limit: 8192,
+					},
+				},
+			},
+		],
+	},
+	resolve: { extensions: ['*', '.js', '.jsx'] },
+	output: {
+		path: path.resolve(__dirname, 'dist/'),
+		publicPath: '/dist/',
+		filename: '[name].bundle.js',
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Production',
+		}),
+	],
 };
