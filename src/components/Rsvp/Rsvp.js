df2bf36475b50e35be59df22505b9ebaa0f7134a
Helen Cadwallader <helencadwallader3@gmail.com>
2020-04-29 15:22:45 +0100

    adding rsvp behind code functionality

--- src/components/Rsvp/Rsvp.js
+++ src/components/Rsvp/Rsvp.js
@@ -10,6 +10,9 @@ const guestDataService =
 const Rsvp = () => {
 	const [guestlist, setGuest] = useState([]);
 	const [guestResponse, setGuestResponse] = useState([]);
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [hasResponded, setHasResponded] = useState(false);
+	const [isAttending, setIsAttending] = useState();
 
 	useEffect(() => {
 		const guests = guestDataService.GetGuestList();
@@ -23,63 +26,123 @@ const Rsvp = () => {
     }
 */
 	const handleGuestChange = (e) => {
-		setGuestResponse({
-			...guestResponse,
-			[e.target.name]: [e.target.value],
-		});
+		const { name, value } = e.target;
+		setGuestResponse({ ...guestResponse, [name]: value });
+		console.log(guestResponse);
+	};
+
+	const handleGuestLogin = (e) => {
+		console.log(e);
+		setIsAuthenticated(true);
+	};
+
+	const handleGuestRsvpSubmission = (e) => {
+		console.log(e);
+		setHasResponded(true);
+		setIsAttending(false);
 	};
 
 	return (
 		<div className="whiteBox">
-			<h1>RSVP form</h1>
-			<form>
-				<input
-					type="text"
-					name="name"
-					id="name"
-					value={guestResponse.name}
-					onChange={handleGuestChange}
-					placeholder="Name"
-				/>
-                <input
-					type="text"
-					name="name"
-					id="name"
-					value={guestResponse.name}
-					onChange={handleGuestChange}
-					placeholder="Name"
-				/>
-                <input
-					type="text"
-					name="name"
-					id="name"
-					value={guestResponse.name}
-					onChange={handleGuestChange}
-					placeholder="Name"
-				/>
-                <input
-					type="text"
-					name="name"
-					id="name"
-					value={guestResponse.name}
-					onChange={handleGuestChange}
-					placeholder="Name"
-				/>
-                <input
-					type="text"
-					name="songchoice"
-					id="songchoice"
-					placeholder="Enter a great song for our evening playlist (optional)"
-				/>
-				{/* {guestlist &&
-					Object.values(guestlist).map((g, index) => {
-						return <p key={index}>{g.name}</p>;
-					})} */}
-				{/* <input type="button" value="Add New Cat" />
-        <input type="submit" value="Submit" />*/}
-			</form>
+			{!isAuthenticated && (
+				<React.Fragment>
+					<h1>Login</h1>
+					<form>
+						<input
+							type="text"
+							name="name"
+							id="name"
+							value={guestResponse.name}
+							onChange={handleGuestChange}
+							onBlur={handleGuestChange}
+							placeholder="Please enter your name"
+						/>
+						<input
+							type="text"
+							name="accessCode"
+							id="accessCode"
+							value={guestResponse.accessCode}
+							onChange={handleGuestChange}
+							onBlur={handleGuestChange}
+							placeholder="Please enter the code from your invite"
+						/>
+						<button type="button" onClick={handleGuestLogin}>
+							Submit
+						</button>
+					</form>
+				</React.Fragment>
+			)}
+			{isAuthenticated && !hasResponded && (
+				<React.Fragment>
+					<h1>RSVP form</h1>
+					<form>
+						<input
+							type="text"
+							name="name"
+							id="name"
+							value={guestResponse.name}
+							onChange={handleGuestChange}
+							placeholder="Name"
+						/>
+						<input
+							type="text"
+							name="isAttending"
+							id="isAttending"
+							value={guestResponse.attending}
+							onChange={handleGuestChange}
+							placeholder="Are you coming?"
+						/>
+						<input
+							type="text"
+							name="starterChoice"
+							id="starterChoice"
+							value={guestResponse.starterChoice}
+							onChange={handleGuestChange}
+							placeholder="What starter would you like?"
+						/>
+						<input
+							type="text"
+							name="mainChoice"
+							id="mainChoice"
+							value={guestResponse.mainChoice}
+							onChange={handleGuestChange}
+							placeholder="What main course would you like?"
+						/>
+						<input
+							type="text"
+							name="songchoice"
+							id="songchoice"
+							value={guestResponse.songchoice}
+							onChange={handleGuestChange}
+							placeholder="Enter a great song for our evening playlist (optional)"
+						/>
+
+						<button
+							type="button"
+							onClick={handleGuestRsvpSubmission}
+						>
+							Submit
+						</button>
+						{/* {guestlist &&
+                         Object.values(guestlist).map((g, index) => {
+                             return <p key={index}>{g.name}</p>;
+                         })} */}
+						{/* <input type="button" value="Add New Cat" />
+             <input type="submit" value="Submit" />*/}
+					</form>
+				</React.Fragment>
+			)}
+			{isAuthenticated && hasResponded && (
+				<h2>
+					Thanks for your RSVP.{' '}
+					{isAttending ? (
+						<span>Can't wait to see you at the wedding!</span>
+					) : (
+						<span>Sorry to hear you can't join us.</span>
+					)}
+				</h2>
+			)}
 		</div>
 	);
 };
-
 export default Rsvp;
