c6884ec58718aa7ab3a0a30db97f57f160a65d8d
Helen Cadwallader <helencadwallader3@gmail.com>
2020-05-06 10:20:34 +0100

    fixing console error relating to timer search for elements when not on home page

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,83 +1,92 @@
-import React, {useEffect} from "react";
-import "./Timer.scss";
-import "../../styles/App.scss"
+import React, { useEffect } from 'react';
+import './Timer.scss';
+import '../../styles/App.scss';
 
 const Timer = () => {
+	const countdown = (endDate) => {
+		let days,
+			hours,
+			minutes,
+			seconds = 0;
 
-    const countdown = (endDate) => {
-        let days, hours, minutes, seconds = 0;
+		endDate = new Date(endDate).getTime();
+		if (isNaN(endDate)) {
+			return;
+		}
 
-        endDate = new Date(endDate).getTime();
-        if (isNaN(endDate)) {
-            return;
-        }
+		const calculate = () => {
+			let startDate = new Date().getTime();
 
-        const calculate = () => {
-            let startDate = new Date().getTime();
+			let timeRemaining = parseInt((endDate - startDate) / 1000);
 
-            let timeRemaining = parseInt((endDate - startDate) / 1000);
+			if (timeRemaining >= 0) {
+				days = parseInt(timeRemaining / 86400);
+				timeRemaining = timeRemaining % 86400;
 
-            if (timeRemaining >= 0) {
-                days = parseInt(timeRemaining / 86400);
-                timeRemaining = (timeRemaining % 86400);
+				hours = parseInt(timeRemaining / 3600);
+				timeRemaining = timeRemaining % 3600;
 
-                hours = parseInt(timeRemaining / 3600);
-                timeRemaining = (timeRemaining % 3600);
+				minutes = parseInt(timeRemaining / 60);
+				timeRemaining = timeRemaining % 60;
 
-                minutes = parseInt(timeRemaining / 60);
-                timeRemaining = (timeRemaining % 60);
+				seconds = parseInt(timeRemaining);
 
-                seconds = parseInt(timeRemaining);
+				if (document.getElementById('days')) {
+					document.getElementById('days').innerHTML = parseInt(
+						days,
+						10
+					);
+					document.getElementById('hours').innerHTML =
+						hours < 10 ? '0' + hours : hours;
+					document.getElementById('minutes').innerHTML =
+						minutes < 10 ? '0' + minutes : minutes;
+					document.getElementById('seconds').innerHTML =
+						seconds < 10 ? '0' + seconds : seconds;
+				}
+			} else {
+				return;
+			}
+		};
+		setInterval(calculate, 1000);
+	};
 
-                document.getElementById("days").innerHTML = parseInt(days, 10);
-                document.getElementById("hours").innerHTML = hours < 10 ? "0" + hours : hours;
-                document.getElementById("minutes").innerHTML = minutes < 10 ? "0" + minutes : minutes;
-                document.getElementById("seconds").innerHTML = seconds < 10 ? "0" + seconds : seconds;
-            } else {
-                return;
-            }
-        }
-        setInterval(calculate, 1000);
-    }
+	useEffect(() => {
+		countdown('08/22/2020 02:00:00 PM');
+	}, ['']);
 
-    useEffect(() => {
-        countdown('08/22/2020 02:00:00 PM');
-    }, [""]);
+	return (
+		<React.Fragment>
+			<div className="Countdown">
+				<span className="Countdown-col">
+					<span className="Countdown-col-element">
+						<strong id="days"></strong>
+						<span>days</span>
+					</span>
+				</span>
 
-  return (
-      <React.Fragment>
-          <div className="Countdown">
-        <span className="Countdown-col">
-          <span className="Countdown-col-element">
-              <strong id="days"></strong>
-              <span>days</span>
-          </span>
-        </span>
+				<span className="Countdown-col">
+					<span className="Countdown-col-element">
+						<strong id="hours"></strong>
+						<span>hours</span>
+					</span>
+				</span>
 
-              <span className="Countdown-col">
-          <span className="Countdown-col-element">
-            <strong id="hours"></strong>
-            <span>hours</span>
-          </span>
-        </span>
+				<span className="Countdown-col">
+					<span className="Countdown-col-element">
+						<strong id="minutes"></strong>
+						<span>minutes</span>
+					</span>
+				</span>
 
-
-              <span className="Countdown-col">
-          <span className="Countdown-col-element">
-            <strong id="minutes"></strong>
-            <span>minutes</span>
-          </span>
-        </span>
-
-              <span className="Countdown-col">
-          <span className="Countdown-col-element">
-            <strong id="seconds"></strong>
-            <span>seconds</span>
-          </span>
-        </span>
-          </div>
-  </React.Fragment>
-  );
+				<span className="Countdown-col">
+					<span className="Countdown-col-element">
+						<strong id="seconds"></strong>
+						<span>seconds</span>
+					</span>
+				</span>
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default Timer;
