55c9dd121a7722b11bebc7975b8836219e2604be
Helen Cadwallader <helencadwallader3@gmail.com>
2020-06-16 21:41:12 +0100

    Mock up dummy service (#9)
    * styling tweaks for device differences rsvp page
    
    * populating dummy data to test locally without needing to hit db

--- src/services/DummyGuestDataService.js
+++ src/services/DummyGuestDataService.js
@@ -1,42 +1,10 @@
 import { MapToGuests } from './guestMapper';
+import { Guests } from '../testData/guests';
 
-const url = 'https://schwanwedding.com/api/';
-
-export const getGuestList = async (guestResponse) => {
-	let authTokenResponse = await fetch(url + 'login', {
-		headers: { 'Content-Type': 'application/json; charset=utf-8' },
-		method: 'POST',
-		body: JSON.stringify({
-			name: guestResponse.name,
-			code: guestResponse.accessCode,
-		}),
-	});
-	let data = await authTokenResponse.json();
-	window.localStorage.setItem('token', data.token);
-	let guestListResponse = await fetch(url + 'group', {
-		headers: {
-			Authorization: `Bearer ${data.token}`,
-		},
-		credentials: 'include',
-		method: 'GET',
-	});
-	let guests = await guestListResponse.json();
-
-	let mappedGuest = MapToGuests(guests);
-	return mappedGuest;
+export const getGuestList = async (guestsResponses) => {
+	return MapToGuests(Guests);
 };
 
 export const submitResponses = (guestsResponses) => {
-	Object.values(guestsResponses).map((guest) => {
-		return fetch(url + 'guests/' + guest.id, {
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${window.localStorage.getItem('token')}`,
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			credentials: 'include',
-			body: JSON.stringify(guest),
-		});
-	});
+	return true;
 };
