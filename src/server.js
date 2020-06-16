d8fee76bf8c09c386dde2ec15b9b14a0222464c1
PSchwan <paul.schwan@hotmail.com>
2020-06-16 21:36:15 +0100

    fixing fonts

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ var app = express();
 // serve everything under the path directly from the directory on the box
 // app.use('/{PATH}', express.static('{DIR_ON_BOX}'));
 app.use('/images', express.static('images'));
+app.use('/fonts', express.static('fonts'));
 app.use('/dist', express.static('dist'));
 
 // for every other path, just give them index.html
