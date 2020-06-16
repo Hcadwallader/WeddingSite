bbe3b2607bd94f765a493419b0a44ae30673388e
Helen Cadwallader <helencadwallader3@gmail.com>
2020-06-16 20:25:50 +0100

    remove unnecessary api page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Accommodation from './components/Accommodation/Accommodation';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
-import ApiTest from './components/ApiTest/ApiTest';
 import Rsvp from './components/Rsvp/Rsvp';
 
 export const App = () => (
@@ -18,7 +17,6 @@ export const App = () => (
 					<Route exact path="/" component={Home} />
 					<Route path="/Venue" component={Venue} />
 					<Route path="/Accommodation" component={Accommodation} />
-					<Route path="/Api" component={ApiTest} />
 					<Route path="/Rsvp" component={Rsvp} />
 					<Route component={Home} />
 					<Redirect to="/" />
