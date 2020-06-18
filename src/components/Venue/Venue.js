c1ddaf116f327c8cc2d74fa8c9056e4a2ec87d30
Helen Cadwallader <helencadwallader3@gmail.com>
2020-06-18 11:58:54 +0100

    Styling fixes venue (#12)
    * styling tweak on venue page on mobile
    
    * styling fixes venue page for chrome and various device issues

--- src/components/Venue/Venue.js
+++ src/components/Venue/Venue.js
@@ -3,7 +3,7 @@ import './Venue.scss';
 import '../../styles/App.scss';
 
 const Venue = () => (
-	<div className="whiteBox pageContainer">
+	<div className="whiteBox venueContainer">
 		<h1>Venue</h1>
 		<div className="row">
 			<div className="column">
